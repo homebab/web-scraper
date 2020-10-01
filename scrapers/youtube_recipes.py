73cebbf0f17311d0f99585c42866e0a328a0c665
meowpunch <jinhoonbae@gmail.com>
2020-10-01 23:21:12 +0900

    JB: backup... I find youtube api... OTL

--- scrapers/youtube_recipes.py
+++ scrapers/youtube_recipes.py
@@ -2,12 +2,14 @@ import json
 import multiprocessing as mp
 import re
 import uuid
+from datetime import datetime
 from functools import reduce
 from time import sleep
 from typing import List
 
 import requests
 from bs4 import BeautifulSoup as bs
+from flask_restx import abort
 from joblib import dump, load
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -26,9 +28,8 @@ class YoutubeRecipeScraper(SeleniumScraper):
             description: string,
             views: number,
             tags: Array<string>,
-            uploaded_at: Date,
             source_url: string,
-            thumbnail_url: string,
+            # thumbnail_url: string,
             owner: string,
             avatar_url: string
         }
@@ -51,13 +52,18 @@ class YoutubeRecipeScraper(SeleniumScraper):
             3. quit driver
         :return: items
         """
-        recipes = self.crawl()
-        # self.s3_manager.save_dict_to_json(
-        #     data=items,
-        #     key="{prefix}/{name}.json".format(prefix=self.prefix, name="item_categories")
-        # )
-        self.driver.quit()
-        return recipes
+        try:
+            data = self.crawl()
+            self.s3_manager.save_dict_to_json(
+                data=data,
+                key="{prefix}/{name}.json".format(prefix=self.prefix, name="youtube-{}".format(data['owner']))
+            )
+            self.driver.quit()
+            return data
+        except Exception as e:
+            self.logger.exception(e, exc_info=True)
+            self.driver.quit()
+            abort(400, custom='[omtm]: the selenium exits with error, {}'.format(e))
 
     def crawl(self) -> dict:
         """
@@ -70,7 +76,7 @@ class YoutubeRecipeScraper(SeleniumScraper):
     def connection(self, url=None) -> None:
         target_url = url if url else self.base_url
         self.driver.get(target_url)
-        self.logger.debug("success to connect with '{url}'".format(url=target_url))
+        self.logger.debug("[omtm]: success to connect with '{url}'".format(url=target_url))
 
     def get_recipes(self) -> dict:
         """
@@ -95,7 +101,7 @@ class YoutubeRecipeScraper(SeleniumScraper):
         return uuid.uuid4()
 
     def get_external_id(self):
-        return self.driver.current_url.split('?')[1].split['&'][0].replace('v=', '')
+        return self.driver.current_url.split('?v=')[1].split('&')[0]
 
     def get_title(self):
         return self.driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
@@ -104,8 +110,8 @@ class YoutubeRecipeScraper(SeleniumScraper):
         return self.driver.find_element_by_xpath('//*[@id="description"]/yt-formatted-string/span[3]').text
 
     def get_views(self):
-        x = self.driver
-        return 0
+        text = self.driver.find_element_by_xpath('//*[@id="count"]/yt-view-count-renderer/span[1]').text
+        return int(''.join(filter(lambda c: c.isdigit(), text)))
 
     def get_tags(self):
         elements = self.driver.find_elements_by_xpath('//*[@id="container"]/yt-formatted-string/a')
@@ -115,7 +121,7 @@ class YoutubeRecipeScraper(SeleniumScraper):
         return self.driver.find_element_by_xpath('//*[@id="text"]/a').text
 
     def get_avatar_url(self):
-        return self.driver.find_element_by_xpath('//*[@id="img"]').get_attribute('src').text
+        return self.driver.find_element_by_xpath('//*[@id="img"]').get_attribute('src')
 
 
 class BaekRecipeScraper(YoutubeRecipeScraper):
@@ -129,7 +135,12 @@ class BaekRecipeScraper(YoutubeRecipeScraper):
 
         if scrap_targets:
             self.connection()
-            self.targets = self.save_targets()
+            try:
+                self.targets = self.save_targets()
+            except Exception as e:
+                self.logger.exception(e, exc_info=True)
+                self.driver.quit()
+                abort(400, custom='[omtm]: the selenium exits with error, {}'.format(e))
         else:
             self.targets = load('resources/targets')
 
@@ -144,36 +155,39 @@ class BaekRecipeScraper(YoutubeRecipeScraper):
             # TODO: not to be ambiguous
             sleep(3)
 
-        recipe_elements = html.find_elements_by_id('thumbnail')
+        recipe_elements = html.find_elements_by_xpath(
+            '//*[@id="contents"]/ytd-playlist-video-renderer')  # html.find_elements_by_id('thumbnail')
+        owner_element = html.find_element_by_id('owner-container')
+
         targets = list(filter(
-            lambda url: url, map(
-                lambda ele: (ele.get_attribute("href"), ele.find_element_by_tag_name('img').get_attribute("src")),
+            lambda url: None not in url, map(
+                lambda ele: {
+                    'source_url': ele.find_element_by_tag_name('a').get_attribute("href"),
+                    'owner': owner_element.find_element_by_id('upload-info').text,
+                    'avatar_url': owner_element.find_element_by_id('avatar').find_element_by_id('img').get_attribute(
+                        'src')
+                },
                 recipe_elements
-            )))[1:]
+            )))
         dump(targets, 'resources/targets')
 
+        self.logger.debug('[omtm]: success to save targets on local, {}'.format(targets))
         return targets
 
     def get_recipe(self, target) -> dict or None:
-        target_url, thumbnail_url = target
-        self.connection(target_url)
+        self.connection(target["source_url"])
 
         WebDriverWait(self.driver, 10).until(
             expected_conditions.presence_of_element_located(
                 (By.XPATH, '//*[@id="description"]/yt-formatted-string/span[3]')
             )
         )
-        # self.fields = ['id', 'external_id', 'title', 'description', 'views',
-        #                        'tags', 'thumbnail_url', 'owner', 'avatar_url']
 
-        sub_dict = {'source_url': target_url, 'thumbnail_url': thumbnail_url}
-        try:
-            return { **self.make_dict(), **sub_dict}
-        except Exception as e:
-            self.logger.exception(e, exc_info=True)
-            return None
+        merged = {**self.make_dict(), **target}
+        self.logger.debug('[omtm]: scrape a recipe, {}'.format(merged))
+        return merged
 
-    def get_recipes(self) -> List[dict]:
+    def get_recipes(self) -> dict:
         """
             1. get all target url
                 - retrieve target urls or scrap again
@@ -184,29 +198,28 @@ class BaekRecipeScraper(YoutubeRecipeScraper):
         :return:
         """
 
-        # targets = self.get_all_target_url()
-
-        # result = []
-        # for target_url in self.targets:
-        #     result.append(self.get_recipe(target_url=target_url))
-        #
-        # print(result)
-        result = filter(lambda d: d, map(self.get_recipe, self.targets))
+        result = filter(lambda d: None not in d, map(self.get_recipe, self.targets))
         print(list(result))
 
         # with mp.Pool(mp.cpu_count()) as p:
         #     result = p.map(self.worker, targets)
 
         # reduced = list(reduce(lambda l, r: l + r, result))
-        return 0
+        return {
+            'platform': 'youtube',
+            'owner': self.targets[0]['owner'],
+            'uploaded_at': datetime.now(),
+            'recipes': list(result)
+        }
 
+    if __name__ == '__main__':
+        a = load('../resources/targets')
+        print(len(a))
+        print(list(filter(lambda x: None in x, a)))
 
-if __name__ == '__main__':
-    # a = load('../resources/targets')
-    # print(a)
+        print(dict([(1, 2), (3, 4)]))
 
-    print(dict([(1, 2), (3, 4)]))
+        # print({1:2} + {1:2})
+        print(''.join(filter(lambda c: c.isdigit(), '조회수 1,234,512회')))
 
-    # print({1:2} + {1:2})
-
-    print(dict(zip([1,2,3],[12,3,4])))
+        print(dict(zip([1, 2, 3], [12, 3, 4])))
