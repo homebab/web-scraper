98c0fc72f42683d604c77515aaebf26c91a09856
meowpunch <jinhoonbae@gmail.com>
2020-10-06 20:29:19 +0900

    JB: tidy up code

--- manager.py
+++ manager.py
@@ -1,13 +1,10 @@
-import os
 import unittest
 
-from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from app.main import create_app
 
 app = create_app()
-
 # app.app_context().push()
 
 manager = Manager(app)
